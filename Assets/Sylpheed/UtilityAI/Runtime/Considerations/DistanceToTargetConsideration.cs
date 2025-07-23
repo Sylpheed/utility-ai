897ef633755c4fea81481321aea47532338330a5
Deng Rodil <deng.rodil@gmail.com>
2025-07-23 19:04:54 +0800

    Added DistanceToTarget consideration

---
+++ Assets/Sylpheed/UtilityAI/Runtime/Considerations/DistanceToTargetConsideration.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Sylpheed.UtilityAI.Sylpheed.UtilityAI.Runtime.Considerations
+{
+    [CreateAssetMenu(fileName = "Distance To Target", menuName = "Utility AI/Consideration/Distance To Target")]
+    public class DistanceToTargetConsideration : Consideration
+    {
+        [Header("Distance To Target")] 
+        [SerializeField] private float _maxDistance;
+
+        protected override float OnEvaluate(Decision decision)
+        {
+            if (decision.Target == null) return 0f;
+            
+            var distance = Vector3.Distance(decision.Agent.transform.position, decision.Target.transform.position);
+            if (distance > _maxDistance) return 0f;
+            
+            return EvaluateCurve(distance, _maxDistance);
+        }
+    }
+}
