804e2111a76d1ad4ab70ca9dabddb1d08bd9a7e6
Deng Rodil <deng.rodil@gmail.com>
2025-08-07 18:22:07 +0800

    Added Fixed Consideration

---
+++ Assets/Sylpheed/UtilityAI/Runtime/Base Considerations/FixedConsideration.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Sylpheed.UtilityAI.Considerations
+{
+    [CreateAssetMenu(fileName = "Fixed", menuName = "Utility AI/Consideration/Fixed")]
+    public class FixedConsideration : Consideration
+    {
+        [SerializeField] [Range(0, 1)] private float _score;
+
+        protected sealed override float OnEvaluate(Decision decision) => _score;
+    }
+}
